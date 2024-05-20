70158d07473d21a07331a9485a3a5f40ce8db028
sirdashasan <sirdashasan@hotmail.com>
2024-05-20 13:49:21 +0300

    metotlar eklendi

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -18,19 +18,30 @@ public class Main {
 
 
     public static boolean hasTeen(int firstAge, int secondAge, int thirdAge) {
-       
+        return (firstAge >= 13 && firstAge <= 19) ||
+                (secondAge >= 13 && secondAge <= 19) ||
+                (thirdAge >= 13 && thirdAge <= 19);
     }
 
     public static boolean isCatPlaying(boolean isSummer, int temp) {
-     
+        int upperLimit = isSummer ? 45 : 35;
+        return temp >= 25 && temp <= upperLimit;
     }
 
     public static double area(double width, double height) {
-       
+        if (width < 0 || height < 0) {
+            return -1;
+        }
+
+        return width * height;
     }
 
     public static double area(double radius) {
-       
+        if (radius < 0) {
+            return -1;
+        }
+
+        return radius * radius * Math.PI;
     }
 }
 
